03f77fec2da5536c50b9c668474b18c4d101633e
Cat <morancathy@gmail.com>
2021-07-19 22:42:47 -0500

    components folder and weatherData file created

---
+++ src/components/weatherData.js
@@ -0,0 +1,32 @@
+const weatherData = [
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg",
+    conditions: "suny",
+    time: "day"
+  },
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg",
+    conditions: "clear",
+    time: "day"
+  },
+  {
+    img:
+      "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg",
+    conditions: "clear",
+    time: "day"
+  },
+  {
+    img:
+      "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg",
+    conditions: "party sunny",
+    time: "night"
+  },
+  {
+    img:
+      "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg",
+    conditions: "clear",
+    time: "in the middle of the night"
+  }
+];
+
+export default weatherData;
