aee3a86133aeca6ad9f77bc11b4103366ad63bfe
Cat <morancathy@gmail.com>
2021-07-19 22:45:14 -0500

    WeatherForecast file

---
+++ src/WeatherForecast.js
@@ -0,0 +1,24 @@
+import React from "react";
+import weatherData from "./components/weatherData.js";
+
+function WeatherForecast(props) {
+  return (
+    <section>
+      {weatherData.map((thing, index) => (
+        <div key={index} className="weather">
+          <img src={thing.img} alt="..." />
+          <p>
+            <span>conditions: </span>
+            {thing.conditions}
+          </p>
+          <p>
+            <span>time: </span>
+            {thing.time}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
+}
+
+export default WeatherForecast;
