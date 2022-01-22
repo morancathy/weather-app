f902e4e5130132c4098c0afd60c7c9f7bcaa78f0
Cat <morancathy@gmail.com>
2022-01-22 13:07:43 -0600

    add image data

---
+++ src/components/imageData.js
@@ -0,0 +1,22 @@
+const imageData = [
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg"
+  },
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg"
+  },
+  {
+    img:
+      "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg"
+  },
+  {
+    img:
+      "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg"
+  },
+  {
+    img:
+      "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg"
+  }
+];
+
+export default imageData;
